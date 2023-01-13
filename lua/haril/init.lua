aedf871ae425a814e890c4cff430a7069368953e
kgsong <kyunggeun.song@seavantage.com>
2023-01-13 10:25:12 +0900

    first commit

---
+++ lua/haril/init.lua
@@ -0,0 +1,64 @@
+print('hello haril!')
+
+-- Set nvim default languages to english
+vim.api.nvim_exec('language en_US', true)
+
+-- copy to clipboard
+vim.api.nvim_exec('set clipboard=unnamedplus', true)
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- copilot
+require('copilot').setup({
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept_word = false,
+      accept_line = false,
+      accept = "<C-J>",
+      next = "<M-]>",
+      prev = "<M-[>",
+    },
+  },
+  filetypes = {
+    yaml = true,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 16.x
+  server_opts_overrides = {},
+})
+
+-- NvimTree toggle F3
+vim.api.nvim_set_keymap('n', '<F3>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
